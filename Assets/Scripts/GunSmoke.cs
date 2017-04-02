5d35eecd1223239eca47ca9b7e6c2659f8d5c851
Thijs-Jan Guelen <thijsguelen22@gmail.com>
2017-04-02 21:49:26 +0200

    Added code to handle a trigger button from the arduino

---
+++ Assets/Scripts/GunSmoke.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSmoke : MonoBehaviour {
+    //public GameObject particle;
+    public ParticleSystem particle;
+    public AudioSource pew;
+    public bool ArduinoFire;
+    private bool AllowTrigger = true;
+
+    public float timer;
+    void Start()
+    {
+        pew = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && AllowTrigger || ArduinoFire && AllowTrigger)
+        {
+            timer = 0;
+            //Instantiate(particle, endOfgun.position, Quaternion.identity);
+            particle.enableEmission = true;
+            pew.Play();
+            AllowTrigger = false;
+            ArduinoFire = false;
+
+            Debug.Log("BAM!");
+        }
+        if(timer > 0.07f)
+        {
+            particle.enableEmission = false;
+        }
+        if(timer > 3)
+        {
+            AllowTrigger = true;
+        }
+    }
+}
