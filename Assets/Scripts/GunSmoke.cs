d109caee1fd0091b3a3de967674d68a45f8ffa9f
thijsguelen22 <thijsguelen22@gmail.com>
2017-04-04 20:20:16 +0200

    added gun sounds, gun physics, gun gun, gun gun gun, gun?

--- Assets/Scripts/GunSmoke.cs
+++ Assets/Scripts/GunSmoke.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GunSmoke : MonoBehaviour {
     //public GameObject particle;
+    public Transform barrelEnd;
+    public Transform barrelBegin;
+    public GameObject bullet;
+    public GameObject bulletHole;
+
+    public float timeBetweenBullets = 3;
     public ParticleSystem particle;
     public AudioSource pew;
     public bool ArduinoFire;
@@ -13,6 +19,7 @@ public class GunSmoke : MonoBehaviour {
     void Start()
     {
         pew = GetComponent<AudioSource>();
+        ArduinoFire = false;
     }
 
     void Update()
@@ -23,17 +30,47 @@ public class GunSmoke : MonoBehaviour {
             timer = 0;
             //Instantiate(particle, endOfgun.position, Quaternion.identity);
             particle.enableEmission = true;
+
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(barrelEnd.position, barrelEnd.up, out hit))
+            {
+                Collider target = hit.collider;
+                float distance = hit.distance; // How far out?
+                Vector3 location = hit.point; // Where did I make impact?
+                GameObject targetGameObject = hit.collider.gameObject; // What's the GameObject?
+
+                print("Hit something!");
+                Debug.DrawLine(transform.position, hit.point);
+                GameObject bulletInstance;
+                GameObject bulletHoleInstance;
+                bulletInstance = GameObject.Instantiate(bullet, hit.point, bullet.transform.rotation);
+                bulletHoleInstance = GameObject.Instantiate(bulletHole, hit.point, bulletHole.transform.rotation);
+                //bulletInstance.velocity = (barrelEnd.forward * 1000000);
+                //bulletInstance.GetComponent<Rigidbody>().AddRelativeForce(hit.point.forward * 1000);
+                Destroy(bulletInstance, 10.5f);
+            }
+
+
+            //old collider method
+            /*
+            GameObject bulletInstance;
+            bulletInstance = GameObject.Instantiate(bullet, barrelEnd.transform.position, barrelEnd.transform.rotation);
+            //bulletInstance.velocity = (barrelEnd.forward * 1000000);
+            bulletInstance.GetComponent<Rigidbody>().AddRelativeForce(barrelEnd.forward * 1000);
+            Destroy(bulletInstance, 10.5f);*/
+
             pew.Play();
             AllowTrigger = false;
             ArduinoFire = false;
 
             Debug.Log("BAM!");
         }
-        if(timer > 0.07f)
+        if (timer > 0.07f)
         {
             particle.enableEmission = false;
         }
-        if(timer > 3)
+        if(timer > timeBetweenBullets)
         {
             AllowTrigger = true;
         }
